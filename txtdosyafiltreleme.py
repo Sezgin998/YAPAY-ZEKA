b3411787b17ca83d0137903d28fded1e299713b9
SEZGİN COŞKUN <83498901+Sezgin998@users.noreply.github.com>
2021-05-15 13:09:13 +0300

    Add files via upload

---
+++ txtdosyafiltreleme.py
@@ -0,0 +1,12 @@
+dosya = open("odev.txt","r")
+dizi = dosya.readlines()
+for i in dizi:
+    temiz_dizi = ''.join(filter(lambda x: not x.isdigit(),i)) 
+    x= temiz_dizi.replace('=',"")
+    y= x.replace('+',"")
+    z= y.replace('-',"")
+    t= z.replace('(',"")
+    u= t.replace(')',"")
+    v= u.replace('?',"")
+    m=v.rstrip("\n")
+    print(m)
